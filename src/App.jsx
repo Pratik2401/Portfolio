e3a189375eb9e57be381288f464c28e0fa2642cb
Pratik Mali <pratikmali242005@gmail.com>
2024-08-07 20:28:08 +0530

    contact-page

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,9 @@ import Project from './Project';
 import Certification from './Certification';
 import About from './About';
 import Contact from './Contact';
-import MouseTrail from '@pjsalita/react-mouse-trail';
 
 function App() {
-  const config = {
-    color: '#cd0ff9',
-    idleAnimation: false,
-    idleAnimationCount: 10,
-    inverted: false,
-    size: 20,
-    trailCount: 30,
-  };
+
 
   return (
     <Router>
