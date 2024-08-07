785d51a259fb60e6b31893010213682f7ccba304
Pratik Mali <pratikmali242005@gmail.com>
2024-08-07 20:03:05 +0530

    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import Navbar from './Navbar'
-import './App.css'
-import Admin from './Admin'
-import Home from './Home'
-import Education from './Education'
-import Myskill from "./Myskill"
-import Project from './Project'
-import Certification from './Certification'
-import About from './About'
-import Contact from './Contact'
-import MouseTrail from "@pjsalita/react-mouse-trail";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import './App.css';
+import Admin from './Admin';
+import Home from './Home';
+import Education from './Education';
+import Myskill from './Myskill';
+import Project from './Project';
+import Certification from './Certification';
+import About from './About';
+import Contact from './Contact';
+import MouseTrail from '@pjsalita/react-mouse-trail';
+
 function App() {
   const pathname = window.location.pathname;
-  
   const config = {
-    color: "#cd0ff9",
+    color: '#cd0ff9',
     idleAnimation: false,
     idleAnimationCount: 10,
     inverted: false,
     size: 20,
     trailCount: 30,
-    }
+  };
+
   return (
-    <>
-      {pathname === "/Portfolio/" ? (
+    <Router basename="/Portfolio">
+      {pathname === '/Portfolio/admin' ? (
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      ) : (
         <>
           <MouseTrail {...config} />
           <Navbar />
@@ -34,12 +41,9 @@ function App() {
           <About />
           <Contact />
         </>
-      ) : (
-        <Admin />
       )}
-    </>
-
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
